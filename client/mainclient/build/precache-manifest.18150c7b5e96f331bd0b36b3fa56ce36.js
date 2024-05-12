323b8f75e9fa1fd21dc7be1ccf55469145132234
12345debdut <debdut.saha.1@gmail.com>
2024-05-12 16:30:25 +0530

    fixed gallery upload

---
+++ client/mainclient/build/precache-manifest.18150c7b5e96f331bd0b36b3fa56ce36.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76c47edb2f02b463809f9165da83d71d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ea3db34e33b7d62773c0",
+    "url": "./static/css/2.25bff47c.chunk.css"
+  },
+  {
+    "revision": "1f8e4e24e4617c3cf7c9",
+    "url": "./static/css/3.83df6e9a.chunk.css"
+  },
+  {
+    "revision": "e86a1acf2297b8ec69f6",
+    "url": "./static/css/4.43eb57d1.chunk.css"
+  },
+  {
+    "revision": "f61617b6eaddb0dc8501",
+    "url": "./static/css/main.bdb339cf.chunk.css"
+  },
+  {
+    "revision": "4785541b448f3aa77230",
+    "url": "./static/js/10.581c822b.chunk.js"
+  },
+  {
+    "revision": "ea3db34e33b7d62773c0",
+    "url": "./static/js/2.448deb1d.chunk.js"
+  },
+  {
+    "revision": "135884a9d2b33e2121f68e32d11a8334",
+    "url": "./static/js/2.448deb1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f8e4e24e4617c3cf7c9",
+    "url": "./static/js/3.f0a2f9e7.chunk.js"
+  },
+  {
+    "revision": "e86a1acf2297b8ec69f6",
+    "url": "./static/js/4.d3dc8ef4.chunk.js"
+  },
+  {
+    "revision": "91563b9bda85f2bf2064",
+    "url": "./static/js/5.e65ef523.chunk.js"
+  },
+  {
+    "revision": "7f74885110e26af173c0",
+    "url": "./static/js/6.36ebc6b2.chunk.js"
+  },
+  {
+    "revision": "8694e71415215ce736a2",
+    "url": "./static/js/7.87e9363a.chunk.js"
+  },
+  {
+    "revision": "aab336b3e8dbab39b8dc",
+    "url": "./static/js/8.6a4d7b6e.chunk.js"
+  },
+  {
+    "revision": "3ed66d93fcbb89f5f8e9",
+    "url": "./static/js/9.c87200d3.chunk.js"
+  },
+  {
+    "revision": "f61617b6eaddb0dc8501",
+    "url": "./static/js/main.305acd03.chunk.js"
+  },
+  {
+    "revision": "80c064d954b7025d1a2c",
+    "url": "./static/js/runtime-main.2b455053.js"
+  },
+  {
+    "revision": "9caf7fd85c5592677fb28fefb07edb2f",
+    "url": "./static/media/about_us.9caf7fd8.webp"
+  },
+  {
+    "revision": "14c590d124662eb50efa4c00f027b79c",
+    "url": "./static/media/fa-brands-400.14c590d1.eot"
+  },
+  {
+    "revision": "3e1b2a654a784ceb385157140b4ccd71",
+    "url": "./static/media/fa-brands-400.3e1b2a65.woff2"
+  },
+  {
+    "revision": "5e8aa9ea0ebcd2218178f554cdd8e6b0",
+    "url": "./static/media/fa-brands-400.5e8aa9ea.ttf"
+  },
+  {
+    "revision": "bfa9c38bd6081dafe7278dedc4aad1d9",
+    "url": "./static/media/fa-brands-400.bfa9c38b.svg"
+  },
+  {
+    "revision": "df02c782834b113d605d8329368737b4",
+    "url": "./static/media/fa-brands-400.df02c782.woff"
+  },
+  {
+    "revision": "285a9d2a28886ad64c4e45cbd733cf7c",
+    "url": "./static/media/fa-regular-400.285a9d2a.ttf"
+  },
+  {
+    "revision": "5623624dd1b017c66c29d1ac69cdcfa3",
+    "url": "./static/media/fa-regular-400.5623624d.woff"
+  },
+  {
+    "revision": "95f13e0be408d117bd3d9b366084a3ef",
+    "url": "./static/media/fa-regular-400.95f13e0b.svg"
+  },
+  {
+    "revision": "aa66d0e0e38c75666e98db33abae955e",
+    "url": "./static/media/fa-regular-400.aa66d0e0.eot"
+  },
+  {
+    "revision": "ac21cac3f22cc9642f5af32e0c750797",
+    "url": "./static/media/fa-regular-400.ac21cac3.woff2"
+  },
+  {
+    "revision": "3ded831d708bf90b2da86756551b5c1c",
+    "url": "./static/media/fa-solid-900.3ded831d.woff"
+  },
+  {
+    "revision": "42e1fbd2cf655a0b44a2dfae9ca2f8c1",
+    "url": "./static/media/fa-solid-900.42e1fbd2.eot"
+  },
+  {
+    "revision": "6ed5e3bc9018d2a03e2f1fadbf8a0a4a",
+    "url": "./static/media/fa-solid-900.6ed5e3bc.svg"
+  },
+  {
+    "revision": "896e20e26ad07dc63c9526ed814117e9",
+    "url": "./static/media/fa-solid-900.896e20e2.ttf"
+  },
+  {
+    "revision": "d6d8d5da9214dc7d46b297672a602d55",
+    "url": "./static/media/fa-solid-900.d6d8d5da.woff2"
+  },
+  {
+    "revision": "833230f39352e58364d305844ab4a571",
+    "url": "./static/media/footer-bg.833230f3.png"
+  },
+  {
+    "revision": "9ba93f63a3e698dcd85f6f56eb190339",
+    "url": "./static/media/home-banner.9ba93f63.png"
+  },
+  {
+    "revision": "448906d7d3fd4b6f9f00129432abf56e",
+    "url": "./static/media/logo-main.448906d7.png"
+  },
+  {
+    "revision": "2cab1ed48bfe4b4c97e034d467f16ae9",
+    "url": "./static/media/profile.2cab1ed4.png"
+  },
+  {
+    "revision": "723b07d7aea8af2dae67af9cf4701735",
+    "url": "./static/media/teacher.723b07d7.webp"
+  }
+]);
